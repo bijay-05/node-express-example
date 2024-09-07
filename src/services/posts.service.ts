3a35fd61635f1c6d3390199fb453000b9ca1ee8f
bijay-05 <hawkeyesniper69@gmail.com>
2024-09-07 19:03:36 +0545

    feat: create posts service

---
+++ src/services/posts.service.ts
@@ -0,0 +1,27 @@
+import PostsModel from "../models/posts.model.js";
+import { IPost } from "../interfaces";
+
+export const getAllPosts = async(): Promise<{posts: IPost[]; message:string}> => {
+    try {
+        const posts = await PostsModel.findAll();
+        return {
+            posts,
+            message: 'Posts fetched successfully'
+        }
+    } catch (error) {
+        console.log('Error from getAllPosts service: ', error);
+        throw error;
+    }
+}
+
+export const createPost = async(post: Omit<IPost, 'id'>): Promise<{createdPost: IPost; message: string} | undefined> => {
+    try {
+        const createdPost = await PostsModel.create(post);
+        return {
+            createdPost,
+            message: 'Post created successully'
+        }
+    } catch (error) {
+        console.log('Error from createPost service: ', error);
+    }
+}
